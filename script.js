92540c8aa7059b301794af448efb5d745a35c856
Sahil Pandey <85783436+pandey-sahil@users.noreply.github.com>
2024-11-14 17:35:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-     // JavaScript to detect device
-        window.onload = function() {
-            if (window.innerWidth <= 768) {  // Mobile devices
-                window.location.href = "responsive.html";
-            } else {  // Desktop devices
-                window.location.href = "index.html";
-            }
-        }
+  
